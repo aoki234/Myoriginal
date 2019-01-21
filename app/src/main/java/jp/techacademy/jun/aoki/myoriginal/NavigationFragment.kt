38b3b6cfc0772de4a12ef8109b564e5593c35581
aoki234 <vamos23467@gmail.com>
2019-01-22 00:08:40 +0900

    ButtonNavigation finished　２

--- app/src/main/java/jp/techacademy/jun/aoki/myoriginal/NavigationFragment.kt
+++ app/src/main/java/jp/techacademy/jun/aoki/myoriginal/NavigationFragment.kt
@@ -9,6 +9,6 @@ import android.view.ViewGroup
 class NavigationFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return  inflater.inflate(R.layout.fragment_navigations, container, false)
+        return inflater.inflate(R.layout.fragment_navigations, container, false)
     }
 }
